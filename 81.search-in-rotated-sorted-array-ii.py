5087acee9f961bace7f9e6ace35bf3f4ee094eec
chemhding <chemhding@gmail.com>
2025-01-24 00:49:04 -0600

    binary search problems

--- 81.search-in-rotated-sorted-array-ii.py
+++ 81.search-in-rotated-sorted-array-ii.py
@@ -8,26 +8,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         l = 0
-        r = len(nums)
+        r = len(nums) - 1
 
-        while l < r:
+        while l <= r:
             mid = l + (r-l)//2
             if nums[mid] == target:
                 return True
-            if nums[l] < nums[mid]:
-                if target < nums[mid]:
-                    r = mid
-                else:
-                    l = mid+1
-            elif nums[mid] < nums[r]:
-                if target > nums[mid]:
-                    l = mid+1
-                else:
-                    r = mid
-            elif nums[l] == nums[mid]:
+            if nums[l] == nums[mid]: # cannot predict where the start is
                 l += 1
-            else:
+            elif nums[mid] == nums[r]:
                 r -= 1
+            elif nums[mid] < nums[r]: # meaning [mid, r] must be ascending
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            else: # meaning [l, mid] must be ascending
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+
             
         return False
 
